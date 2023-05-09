a67c35dcac6463d8dc1815bd986e06e3588ba1b4
hoangvo1311 <hoangvo1311@gmail.com>
2023-05-09 02:00:03 -0400

    Implement List Accounts

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -138,4 +138,14 @@ class TestAccountService(TestCase):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+    
+    def test_get_account_list(self):
+        """It should Get a list of accounts"""
+        self._create_accounts(3)
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 3)
+
+
 
